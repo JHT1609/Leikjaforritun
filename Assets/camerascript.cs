81beff2f1fe322931d2a1dc71ddb50d1ea36bb2e
Tumi <tumithorber@gmail.com>
2019-09-26 16:06:56 +0000

    bætti við armor og hurð er með animation

--- Assets/camerascript.cs
+++ Assets/camerascript.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class camerascript : MonoBehaviour
 {
+
+    public float moveSpeed = 0f;
+
+    Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
