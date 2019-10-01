5da407011236100f6a62142015522492842ea282
Jóhannes Helgi Tómasson <johannesto245@nemi.tskoli.is>
2019-10-01 11:39:27 +0000

    Commit 1/10 -4

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 movementDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ProcessInputs() {
+        movementDirection = new Vector2(player.GetAxis("Horizontal"), player.GetAxis("Vertical"));
+    }
+}
